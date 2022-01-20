7ebbcfb8e7a66a9c9f60e4332a3db46e3a6d9fc9
Enrique Soriano <jenriquesoriano@guadaltel.com>
2022-01-20 15:43:50 +0100

    local_old

---
+++ validator/js/config.js
@@ -0,0 +1,19 @@
+var captchaEnabled = false;
+var betaBanner = false;
+var labelStaging = false;
+// PROD
+//var serverURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/rest/";
+//var serverRealURL = "https://inspire.ec.europa.eu/validator/v2/";
+//var serverCaptchaURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/captcha/verify.php";
+// SANDBOX
+// var serverURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/rest/";
+// var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
+// var serverCaptchaURL = "https://inspire-sandbox.jrc.ec.europa.eu/validator-testbed/captcha/verify.php";
+// STAGING
+//var serverURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
+//var serverRealURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/v2/";
+//var serverCaptchaURL = "http://staging-inspire-validator.eu-west-1.elasticbeanstalk.com/validator/captcha/verify.php";
+// PROD CONTRACTOR
+var serverURL = "http://localhost:8090/validator/v2/";
+var serverRealURL = "http://localhost:8090/validator/v2/";
+var serverCaptchaURL = "http://localhost:8090/validator/v2/validator/captcha/verify.php";
